f7b520124da9c921a03904074bace1750a72f148
charly-poket <alu0100844665@ull.edu.es>
2017-03-13 10:15:07 +0000

    mas pasos

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 ##Práctica:Evaluar Manejo de Cookies y Sessions en ExpressJS
 
+__Hitos__
+Use el repo de GitHub dado por la asignación de esta tarea
+Cree un servidor que use para proveer autenticación a los HTML generados a partir de un libro de prueba
+Encripte los passwords con bcrypt-nodejs.
+Guarde las parejas usuario: password en un fichero users.json
+Use un formulario form para obtener el usuario y la clave
+Use vistas en ejs o pug para el programa
+Despliegue la aplicación en Heroku o en la máquina virtual del iaas
 
 
 
--- staticauth.js
+++ staticauth.js
@@ -4,3 +4,18 @@ let express = require('express'),
   app = express(),
   session = require('express-session');
 let cookieParser = require('cookie-parser');
+let path = require('path');
+let util = require("util");
+let bcrypt = require("bcrypt-nodejs");
+let hash = bcrypt.hashSync("amyspassword");
+
+let instructions = `
+Visit these urls in the browser:
+<ul>
+  <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
+  <li> <a href="http://localhost:3000/content/chapter1.html">localhost:3000/content/chapter1.html</a> </li>
+  <li> <a href="http://localhost:3000/login?username=juan&password=juanspassword">localhost:3000/login?username=juan&password=juanspassword</a> </li>
+  <li> <a href="http://localhost:3000/login?username=amy&password=amyspassword">localhost:3000/login?username=amy&password=amyspassword</a> </li>
+  <li> <a href="http://localhost:3000/logout">localhost:3000/logout</a> </li>
+</ul>
+`;
